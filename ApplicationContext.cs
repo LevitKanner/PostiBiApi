e5f0bf4abd6e5910ff039450b3bd045292b224d0
Levit Kanner <lkanner21@gmail.com>
2022-07-13 20:42:37 +0000

    Adds identity for user management

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -1,13 +1,13 @@
 using Api.Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api;
 
-public class ApplicationContext: DbContext
+public class ApplicationContext : IdentityDbContext<ApplicationUser>
 {
-    public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+    public ApplicationContext(DbContextOptions options) : base(options)
     {
-        
     }
 
     public DbSet<Post>? Posts { get; set; }
