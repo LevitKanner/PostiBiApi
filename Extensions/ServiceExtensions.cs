6ca3ed3318d3a1d3420f4051f91e1d0a0afff348
Levit Kanner <lkanner21@gmail.com>
2022-07-13 20:43:08 +0000

    Configures identity

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,7 +1,9 @@
 using Api.Contracts;
 using Api.Contracts.ServiceContracts;
+using Api.Entities.Models;
 using Api.Repositories;
 using Api.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Extensions;
@@ -19,12 +21,26 @@ public static class ServiceExtensions
             builder => builder.UseNpgsql(configuration.GetConnectionString("DbConnection"))
                 .UseSnakeCaseNamingConvention()
                 .EnableSensitiveDataLogging());
-    
+
 
     public static void ConfigureRepositoryManager(this IServiceCollection services) =>
         services.AddScoped<IRepositoryManager, RepositoryManager>();
 
     public static void ConfigureServiceManager(this IServiceCollection services) =>
         services.AddScoped<IServiceManager, ServiceManager>();
-    
+
+    public static void ConfigureIdentity(this IServiceCollection services)
+    {
+        var builder = services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 10;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationContext>()
+            .AddDefaultTokenProviders();
+    }
 }
