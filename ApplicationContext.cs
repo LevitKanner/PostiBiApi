192d6631bf978be5fb4b89800af168f487f4c2f2
Levit Kanner <lkanner21@gmail.com>
2022-07-16 07:11:42 +0000

    Add identity

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -1,4 +1,5 @@
 using Api.Entities.Models;
+using Api.Repositories.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,12 @@ public class ApplicationContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationContext(DbContextOptions options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new RoleConfiguration());
     }
 
     public DbSet<Post>? Posts { get; set; }
