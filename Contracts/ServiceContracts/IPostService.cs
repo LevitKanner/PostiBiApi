3c9f75934933981576aa81699c2505a8a9a4df2a
Levit Kanner <lkanner21@gmail.com>
2022-07-16 08:50:13 +0000

    Add post service

--- Contracts/ServiceContracts/IPostService.cs
+++ Contracts/ServiceContracts/IPostService.cs
@@ -1,6 +1,9 @@
-namespace Api.Contracts;
+using Api.Entities.Models;
+
+namespace Api.Contracts.ServiceContracts;
 
 public interface IPostService
 {
-    
+    IEnumerable<Post> GetUserPosts(string userId, bool trackChanges);
+    Post? GetPost(string userId, int postId, bool trackChanges);
 }
--- Services/PostService.cs
+++ Services/PostService.cs
@@ -1,4 +1,6 @@
 using Api.Contracts;
+using Api.Contracts.ServiceContracts;
+using Api.Entities.Models;
 
 namespace Api.Services;
 
@@ -9,5 +11,17 @@ public class PostService: IPostService
     public PostService(IRepositoryManager repositoryManager)
     {
         _repositoryManager = repositoryManager;
+    }
+
+    public IEnumerable<Post> GetUserPosts(string userId, bool trackChanges)
+    {
+        var posts = _repositoryManager.PostRepository.GetUserPosts(userId, trackChanges);
+        return posts;
+    }
+
+    public Post? GetPost(string userId, int postId, bool trackChanges)
+    {
+        var post = _repositoryManager.PostRepository.GetPost(userId, postId, trackChanges);
+        return post;
     }
 }
