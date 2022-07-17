8f2503f8931501d7e9ed6097ee6d2de576d0c118
Levit Kanner <lkanner21@gmail.com>
2022-07-17 07:54:56 +0000

    Add update and delete post to post service

--- Contracts/ServiceContracts/IPostService.cs
+++ Contracts/ServiceContracts/IPostService.cs
@@ -1,9 +1,13 @@
+using Api.Entities.Dtos;
 using Api.Entities.Models;
 
 namespace Api.Contracts.ServiceContracts;
 
 public interface IPostService
 {
-    IEnumerable<Post> GetUserPosts(string userId, bool trackChanges);
+    IEnumerable<Post> GetAllPost(bool trackChanges);
+    IEnumerable<PostDto> GetUserPosts(string userId, bool trackChanges);
     Post? GetPost(string userId, int postId, bool trackChanges);
+    Post CreatePost(string userId, CreatePostDto createPostDto);
+    void DeletePost(Post post);
 }
--- Services/PostService.cs
+++ Services/PostService.cs
@@ -1,27 +1,51 @@
 using Api.Contracts;
 using Api.Contracts.ServiceContracts;
+using Api.Entities.Dtos;
 using Api.Entities.Models;
+using AutoMapper;
 
 namespace Api.Services;
 
-public class PostService: IPostService
+public class PostService : IPostService
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly IMapper _mapper;
 
-    public PostService(IRepositoryManager repositoryManager)
+    public PostService(IRepositoryManager repositoryManager, IMapper mapper)
     {
         _repositoryManager = repositoryManager;
+        _mapper = mapper;
     }
 
-    public IEnumerable<Post> GetUserPosts(string userId, bool trackChanges)
+    public IEnumerable<Post> GetAllPost(bool trackChanges)
+    {
+        var posts = _repositoryManager.PostRepository.GetAllPosts(trackChanges);
+        return posts;
+    }
+
+    public IEnumerable<PostDto> GetUserPosts(string userId, bool trackChanges)
     {
         var posts = _repositoryManager.PostRepository.GetUserPosts(userId, trackChanges);
-        return posts;
+        return _mapper.Map<IEnumerable<PostDto>>(posts);
     }
 
     public Post? GetPost(string userId, int postId, bool trackChanges)
     {
         var post = _repositoryManager.PostRepository.GetPost(userId, postId, trackChanges);
         return post;
+    }
+
+    public Post CreatePost(string userId, CreatePostDto createPostDto)
+    {
+        var post = _mapper.Map<Post>(createPostDto);
+        _repositoryManager.PostRepository.CreatePost(userId, post);
+        _repositoryManager.Save();
+        return post;
+    }
+
+    public void DeletePost(Post post)
+    {
+        _repositoryManager.PostRepository.DeletePost(post);
+        _repositoryManager.Save();
     }
 }
