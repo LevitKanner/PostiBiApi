8d415dd7b5f4d6e91d954b865117bcf64eacd11d
Levit Kanner <lkanner21@gmail.com>
2022-07-17 07:53:51 +0000

    Adds create and delete post to post repository

--- Contracts/RepositoryContracts/IPostRepository.cs
+++ Contracts/RepositoryContracts/IPostRepository.cs
@@ -4,7 +4,10 @@ namespace Api.Contracts.RepositoryContracts;
 
 public interface IPostRepository
 {
+    IEnumerable<Post> GetAllPosts(bool trackChanges);
     IEnumerable<Post> GetUserPosts(string userId, bool trackChanges);
     Post? GetPost(string userId, int postId, bool trackChanges);
+    void CreatePost(string userId, Post post);
+    void UpdatePost(Post post);
     void DeletePost(Post post);
 }
--- Repositories/PostRepository.cs
+++ Repositories/PostRepository.cs
@@ -1,6 +1,6 @@
-using Api.Contracts;
 using Api.Contracts.RepositoryContracts;
 using Api.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Repositories;
 
@@ -10,11 +10,27 @@ public class PostRepository : RepositoryBase<Post>, IPostRepository
     {
     }
 
+    public IEnumerable<Post> GetAllPosts(bool trackChanges) =>
+        FindAll(trackChanges).OrderBy(post => post.CreatedAt)
+            .Include(post => post.User)
+            .ToList();
+
+
     public IEnumerable<Post> GetUserPosts(string userId, bool trackChanges) =>
-        FindByCondition(post => post.UserId == userId, trackChanges).OrderBy(post => post.CreatedAt);
-    
+        FindByCondition(post => post.UserId == userId, trackChanges)
+            .Include(post => post.User)
+            .OrderBy(post => post.CreatedAt);
+
     public Post? GetPost(string userId, int postId, bool trackChanges) =>
         FindByCondition(post => post.UserId == userId && post.Id == postId, trackChanges).SingleOrDefault();
+
+    public void CreatePost(string userId, Post post)
+    {
+        post.UserId = userId;
+        Create(post);
+    }
+
+    public void UpdatePost(Post post) => Update(post);
 
     public void DeletePost(Post post) => Delete(post);
 }
