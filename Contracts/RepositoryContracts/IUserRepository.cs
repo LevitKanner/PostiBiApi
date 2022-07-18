c7e9ba22b598dfa6d214d4e6fc565a771581c7d2
Levit Kanner <lkanner21@gmail.com>
2022-07-18 19:56:40 +0000

    Create user repository

--- Contracts/RepositoryContracts/IUserRepository.cs
+++ Contracts/RepositoryContracts/IUserRepository.cs
@@ -1,6 +1,12 @@
+using Api.Entities.Models;
+
 namespace Api.Contracts.RepositoryContracts;
 
 public interface IUserRepository
 {
-    
+    IEnumerable<ApplicationUser> GetAllUsers();
+    Task<ApplicationUser?> GetUser(string userId);
+    void FollowUser(string userId, string userToFollow);
+    void UnFollowUser(UserFollowing following);
+    UserFollowing? GetFollowing(string userId, string following);
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,51 @@
+using Api.Contracts.RepositoryContracts;
+using Api.Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Repositories;
 
-public class UserRepository
+public class UserRepository : RepositoryBase<UserFollowing>, IUserRepository
 {
-    
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ApplicationContext _context;
+
+    public UserRepository(UserManager<ApplicationUser> userManager, ApplicationContext context) : base(context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+    public IEnumerable<ApplicationUser> GetAllUsers()
+    {
+        var users = _userManager.Users.Include(user => user.Posts).ToList();
+        return users;
+    }
+
+    public async Task<ApplicationUser?> GetUser(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        return user;
+    }
+
+    public void FollowUser(string userId, string userToFollow)
+    {
+        Create(new UserFollowing
+        {
+            UserFollowedId = userToFollow,
+            FollowDate = DateTime.UtcNow,
+            UserId = userId,
+        });
+    }
+
+    public void UnFollowUser(UserFollowing following)
+    {
+        Delete(following);
+    }
+
+    public UserFollowing? GetFollowing(string userId, string following)
+    {
+        return _context.UserFollowings?.FirstOrDefault(userFollowing =>
+            userFollowing.UserId.Equals(userId) && userFollowing.UserFollowedId.Equals(userFollowing));
+    }
 }
