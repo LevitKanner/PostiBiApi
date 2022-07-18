1f291812497c0e4780e1228ce6a543187efe70e2
Levit Kanner <lkanner21@gmail.com>
2022-07-18 19:59:18 +0000

    Add user service manager to service manager

--- Contracts/ServiceContracts/IServiceManager.cs
+++ Contracts/ServiceContracts/IServiceManager.cs
@@ -3,5 +3,6 @@ namespace Api.Contracts.ServiceContracts;
 public interface IServiceManager
 {
    IPostService PostService { get; }
+   IUserService UserService { get; }
    IAuthenticationService AuthenticationService { get; }
 }
--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -10,15 +10,18 @@ public class ServiceManager : IServiceManager
 {
     private readonly Lazy<IPostService> _postService;
     private readonly Lazy<IAuthenticationService> _authService;
+    private readonly Lazy<IUserService> _userService;
 
     public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper, IConfiguration configuration,
         UserManager<ApplicationUser> userManager)
     {
-        _postService = new Lazy<IPostService>(() => new PostService(repositoryManager));
+        _postService = new Lazy<IPostService>(() => new PostService(repositoryManager, mapper, userManager));
         _authService =
             new Lazy<IAuthenticationService>(() => new AuthenticationService(mapper, userManager, configuration));
+        _userService = new Lazy<IUserService>(() => new UserService(repositoryManager, mapper));
     }
 
     public IPostService PostService => _postService.Value;
+    public IUserService UserService => _userService.Value;
     public IAuthenticationService AuthenticationService => _authService.Value;
 }
