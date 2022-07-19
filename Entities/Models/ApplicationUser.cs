71f68c1894a0cc1ca4861f7e1b296f9ea901e437
Levit Kanner <lkanner21@gmail.com>
2022-07-19 14:26:19 +0000

    Add refresh token and expiry date props to Appuser

--- Entities/Models/ApplicationUser.cs
+++ Entities/Models/ApplicationUser.cs
@@ -6,6 +6,8 @@ public class ApplicationUser : IdentityUser
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryDate { get; set; }
     public ICollection<Post>? Posts { get; set; }
     public ICollection<UserFollowing>? UserFollowings { get; set; }
 }
