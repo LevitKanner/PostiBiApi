64be086b444ee5f9cdfbbab3d43d1ca46f9fb210
Levit Kanner <lkanner21@gmail.com>
2022-07-19 14:27:18 +0000

    Implements refresh token generation interface

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -1,5 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Api.Contracts.ServiceContracts;
 using Api.Entities.Dtos;
@@ -41,12 +42,20 @@ public class AuthenticationService : IAuthenticationService
         return _user != null && await _userManager.CheckPasswordAsync(_user, userForAuth.Password);
     }
 
-    public async Task<string> CreateToken()
+    public async Task<TokenDto> CreateToken(bool populateExp)
     {
         var signingCredentials = GetSigningCredentials();
         var claims = await GetClaims();
         var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
-        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        
+        var refreshToken = GenerateRefreshToken();
+        _user.RefreshToken = refreshToken;
+        
+        if (populateExp) 
+            _user.RefreshTokenExpiryDate = DateTime.Now.AddDays(7);
+        await _userManager.UpdateAsync(_user);
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        return new TokenDto(accessToken, refreshToken);
     }
 
     private SigningCredentials GetSigningCredentials()
@@ -80,5 +89,41 @@ public class AuthenticationService : IAuthenticationService
             signingCredentials: signingCredentials
         );
         return tokenOptions;
+    }
+
+    private static string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetValue<string>("secretKey"))),
+            ValidateLifetime = true,
+            ValidIssuer = jwtSettings["ValidIssuer"],
+            ValidAudience = jwtSettings["ValidAudience"]
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+        
+        if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+            !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+        return principal;
     }
 }
