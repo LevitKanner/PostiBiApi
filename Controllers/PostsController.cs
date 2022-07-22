fd4729b7dbf7a93d9e2f2295b22426c7e3c916f6
Levit Kanner <lkanner21@gmail.com>
2022-07-22 10:15:34 +0000

    Add get all posts and post of user followings'

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -4,9 +4,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
+
 [Authorize]
 [ApiController]
-[Route("api/users/{userId}/posts")]
+[Route("api/[controller]/[action]")]
 public class PostsController : ControllerBase
 {
     private readonly IServiceManager _serviceManager;
@@ -16,39 +17,53 @@ public class PostsController : ControllerBase
         _serviceManager = serviceManager;
     }
 
+    [HttpGet]
+    public IActionResult GetAllPosts()
+    {
+        var posts = _serviceManager.PostService.GetAllPost(false);
+        return Ok(new Response(StatusCodes.Status200OK, "Success", posts));
+    }
 
     [HttpGet]
-    public async Task<IActionResult> GetUserPostsAsync(string userId)
+    public async Task<IActionResult> GetUserPostsAsync([FromQuery] string userId)
     {
         var posts = await _serviceManager.PostService.GetUserPosts(userId, false);
         return Ok(new Response(StatusCodes.Status200OK, "Success", posts));
     }
 
-    [HttpGet("{postId:int}")]
-    public async Task<IActionResult> GetPostAsync(string userId, int postId)
+    [HttpGet]
+    public IActionResult GetPostAsync([FromQuery] int postId)
     {
-        var post = _serviceManager.PostService.GetPost(userId, postId, false);
+        var post = _serviceManager.PostService.GetPost(postId, false);
         return Ok(new Response(StatusCodes.Status200OK, "Success", post));
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreatePost([FromBody] CreatePostDto createPostDto, string userId)
+    public async Task<IActionResult> CreatePost([FromBody] CreatePostDto createPostDto, [FromQuery] string userId)
     {
         var createdPost = await _serviceManager.PostService.CreatePost(userId, createPostDto);
         return Ok(new Response(StatusCodes.Status200OK, "Success", createdPost));
     }
 
-    [HttpPut("{postId:int}")]
-    public async Task<IActionResult> UpdatePost([FromBody] UpdatePostDto updatePostDto, string userId, int postId)
+    [HttpPut]
+    public IActionResult UpdatePost([FromBody] UpdatePostDto updatePostDto,
+        [FromQuery] int postId)
     {
-        var post = await _serviceManager.PostService.GetPost(userId, postId, true);
-        return Ok();
+        _serviceManager.PostService.UpdatePost(postId, updatePostDto);
+        return Ok(new Response(StatusCodes.Status200OK, "Success", null));
     }
 
-    [HttpDelete("{postId:int}")]
-    public IActionResult DeletePost(string userId, int postId)
+    [HttpDelete]
+    public IActionResult DeletePost([FromQuery] int postId)
     {
-        _serviceManager.PostService.DeletePost(userId, postId);
-        return NoContent();
+        _serviceManager.PostService.DeletePost(postId);
+        return Ok(new Response(StatusCodes.Status204NoContent, "Success", null));
+    }
+
+    [HttpGet]
+    public IActionResult GetFollowingPosts(string userId)
+    {
+        var posts = _serviceManager.PostService.GetFollowingPosts(userId);
+        return Ok(new Response(StatusCodes.Status200OK, "Success", posts));
     }
 }
