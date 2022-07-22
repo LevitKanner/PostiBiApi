c77b144dd6e3858641166a3e6344c077ee162f37
Levit Kanner <lkanner21@gmail.com>
2022-07-22 09:52:15 +0000

    Changes signature of get post interface

--- Contracts/RepositoryContracts/IPostRepository.cs
+++ Contracts/RepositoryContracts/IPostRepository.cs
@@ -6,7 +6,7 @@ public interface IPostRepository
 {
     IEnumerable<Post> GetAllPosts(bool trackChanges);
     IEnumerable<Post> GetUserPosts(string userId, bool trackChanges);
-    Post? GetPost(string userId, int postId, bool trackChanges);
+    Post? GetPost(int postId, bool trackChanges);
     void CreatePost(string userId, Post post);
     void UpdatePost(Post post);
     void DeletePost(Post post);
