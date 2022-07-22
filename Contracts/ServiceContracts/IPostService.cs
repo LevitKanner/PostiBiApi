a624f090e756da0145f0fcf686bc9c594593214b
Levit Kanner <lkanner21@gmail.com>
2022-07-22 07:49:12 +0000

    Add interface to get posts of a user's followings

--- Contracts/ServiceContracts/IPostService.cs
+++ Contracts/ServiceContracts/IPostService.cs
@@ -5,9 +5,10 @@ namespace Api.Contracts.ServiceContracts;
 
 public interface IPostService
 {
-    IEnumerable<Post> GetAllPost(bool trackChanges);
+    IEnumerable<PostDto> GetAllPost(bool trackChanges);
     Task<IEnumerable<PostDto>> GetUserPosts(string userId, bool trackChanges);
     Task<PostDto?> GetPost(string userId, int postId, bool trackChanges);
     Task<PostDto> CreatePost(string userId, CreatePostDto createPostDto);
     Task DeletePost(string userId, int postId);
+    IEnumerable<PostDto> GetFollowingPosts(string userId);
 }
