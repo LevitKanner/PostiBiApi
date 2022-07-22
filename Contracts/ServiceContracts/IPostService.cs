0c5b394cdc434d386f336a58099c4f3defc9b85f
Levit Kanner <lkanner21@gmail.com>
2022-07-22 09:54:00 +0000

    Add update post, changes signature of get and delete post in Post service interface

--- Contracts/ServiceContracts/IPostService.cs
+++ Contracts/ServiceContracts/IPostService.cs
@@ -7,8 +7,9 @@ public interface IPostService
 {
     IEnumerable<PostDto> GetAllPost(bool trackChanges);
     Task<IEnumerable<PostDto>> GetUserPosts(string userId, bool trackChanges);
-    Task<PostDto?> GetPost(string userId, int postId, bool trackChanges);
+    PostDto? GetPost(int postId, bool trackChanges);
     Task<PostDto> CreatePost(string userId, CreatePostDto createPostDto);
-    Task DeletePost(string userId, int postId);
+    void UpdatePost(int postId, UpdatePostDto updatePostDto);
+    void DeletePost(int postId);
     IEnumerable<PostDto> GetFollowingPosts(string userId);
 }
