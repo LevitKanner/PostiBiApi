999da494f8a55f67000990062fd02e2538d11977
Levit Kanner <lkanner21@gmail.com>
2022-07-24 22:47:33 +0000

    Add user email to user dto

--- Entities/Dtos/UserDto.cs
+++ Entities/Dtos/UserDto.cs
@@ -1,3 +1,3 @@
 namespace Api.Entities.Dtos;
 
-public record UserDto(string Id, string Username, string LastName, string FirstName);
+public record UserDto(string Id, string Username, string LastName, string FirstName, string Email);
